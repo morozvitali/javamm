3a74c8ac22f501a958724e02befe7e683015142d
morozvitali <morozvitali99@gmail.com>
2024-09-30 09:56:58 +0300

    Task 70 Creating Base Compiller classes
    compiller module -> component ->
    compiller module -> compiller -> model

--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module javamm.compiler {
 
     requires javamm.code;
+
+    exports academy.devonline.javamm.compiler;
+    exports academy.devonline.javamm.compiler.model;
+    exports academy.devonline.javamm.compiler.component;
 }
